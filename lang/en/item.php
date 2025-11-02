4bd8a2ccf0d212261bc8bd8e53ce8140800d57d6
perdidev <perdidev@gmail.com>
2025-11-02 13:29:45 +0800

    locale: init locale

---
+++ lang/en/item.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Item",
+    'nav' => "Item",
+    'fields' => [
+        'name' => 'Item Name',
+        'category' => 'Category',
+        'item_price' => 'Price'
+    ],
+];
---
+++ lang/id/item.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Item",
+    'nav' => "Item",
+    'fields' => [
+        'name' => 'Nama Item',
+        'category' => 'Kategori',
+        'item_price' => 'Harga'
+    ],
+];
